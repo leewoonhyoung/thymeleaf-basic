49d0cce3245e4a3435cc8c384154998f87f2f093
leewoonhyoung <wh3619@gmail.com>
2022-02-18 14:35:13 +0900

    thymeleaf study

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,91 @@
+package hello.thymeleaf.basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("text-basic")
+    public String textBasic(Model model){
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("text-unescaped")
+    public String texUnescaped(Model model){
+        model.addAttribute("data", "<b>Hello Data</b>");
+        return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users",list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+
+
+    }
+
+    @GetMapping("basic-objects")
+    public String basicObject(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    public static class HelloBean{
+
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+
+    @Data
+    public static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age){
+            this.username = username;
+            this.age = age;
+        }
+    }
+
+
+
+}
